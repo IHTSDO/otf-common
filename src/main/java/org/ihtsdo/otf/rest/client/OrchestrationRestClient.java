6988b9fbc5a1744cbc99a1549fada2209f7b4971
Peter G. Williams <pwi@ihtsdo.org>
2015-07-30 12:30:48 +0100

    Orchestration Rest Client needs to use json content type

--- src/main/java/org/ihtsdo/otf/rest/client/OrchestrationRestClient.java
+++ src/main/java/org/ihtsdo/otf/rest/client/OrchestrationRestClient.java
@@ -3,41 +3,30 @@ package org.ihtsdo.otf.rest.client;
 
 import java.io.IOException;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import org.ihtsdo.otf.rest.client.resty.RestyHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import us.monoid.json.JSONArray;
 import us.monoid.json.JSONException;
 import us.monoid.json.JSONObject;
-import us.monoid.web.BinaryResource;
-import us.monoid.web.JSONResource;
 
 public class OrchestrationRestClient {
 
 	public static final String ANY_CONTENT_TYPE = "*/*";
+	public static final String JSON_CONTENT_TYPE = "application/json";
 	public static final String VALIDATION_ENDPOINT = "REST/ts/validate";
 
 	private final String orchestrationUrl;
 	private final RestyHelper resty;
-	private final Gson gson;
-
-	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public OrchestrationRestClient(String orchestrationUrl, String username, String password) {
 		this.orchestrationUrl = orchestrationUrl;
 		this.resty = new RestyHelper(ANY_CONTENT_TYPE);
 		resty.authenticate(orchestrationUrl, username, password.toCharArray());
-		gson = new GsonBuilder().setPrettyPrinting().create();
 	}
 
 	public void validate(String branchPath) throws JSONException, IOException {
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("branchPath", branchPath);
-		resty.json(orchestrationUrl + VALIDATION_ENDPOINT, RestyHelper.content((jsonObject), ANY_CONTENT_TYPE));
+		resty.json(orchestrationUrl + VALIDATION_ENDPOINT, RestyHelper.content((jsonObject), JSON_CONTENT_TYPE));
 	}
 
 
