ebb936c3ea16160a7b4d2ddd52f5209e4225cef8
Kai Kewley <kaikewley@gmail.com>
2015-08-07 15:15:05 +0100

    Orchestration client can retrieve validation report.

--- src/main/java/org/ihtsdo/otf/rest/client/OrchestrationRestClient.java
+++ src/main/java/org/ihtsdo/otf/rest/client/OrchestrationRestClient.java
@@ -1,23 +1,22 @@
 package org.ihtsdo.otf.rest.client;
 
-import java.io.IOException;
-
 import org.ihtsdo.otf.rest.client.resty.RestyHelper;
-import org.ihtsdo.otf.rest.client.resty.RestyServiceHelper;
-import org.ihtsdo.otf.rest.exception.BusinessServiceException;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.monoid.json.JSONException;
-import us.monoid.json.JSONObject;
 import us.monoid.web.JSONResource;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class OrchestrationRestClient {
 
 	public static final String ANY_CONTENT_TYPE = "*/*";
-	public static final String JSON_CONTENT_TYPE = "application/json";
-	public static final String VALIDATION_ENDPOINT = "REST/ts/validate";
+	public static final String VALIDATIONS_ENDPOINT = "REST/termserver/validations";
 
 	private final String orchestrationUrl;
 	private final RestyHelper resty;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public OrchestrationRestClient(String orchestrationUrl, String username, String password) {
 		this.orchestrationUrl = orchestrationUrl;
@@ -25,12 +24,20 @@ public class OrchestrationRestClient {
 		resty.authenticate(orchestrationUrl, username, password.toCharArray());
 	}
 
-	public void validate(String branchPath) throws JSONException, IOException, BusinessServiceException {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("branchPath", branchPath);
-		JSONResource response = resty.json(orchestrationUrl + VALIDATION_ENDPOINT, RestyHelper.content((jsonObject), JSON_CONTENT_TYPE));
-		RestyServiceHelper.ensureSuccessfull(response);
+	public String retrieveValidation(String branchPath) throws IOException, JSONException {
+		try {
+			final String path = orchestrationUrl + VALIDATIONS_ENDPOINT + "/" + branchPath + "/latest";
+			logger.info("Path '{}'", path);
+			final JSONResource json = resty.json(path);
+			final String httpStatus = json.getHTTPStatus().toString();
+			logger.info("Path '{}', response '{}'", path, httpStatus);
+			if (httpStatus.startsWith("2")) {
+				return json.toObject().toString();
+			}
+		} catch (FileNotFoundException e) {
+			// Swallowing this. Gulp
+		}
+		return null;
 	}
-
 
 }
