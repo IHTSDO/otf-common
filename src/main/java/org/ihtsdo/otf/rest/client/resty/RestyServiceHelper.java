c0cc2abb665f1342b7003b5647636f97a6781d69
Peter G. Williams <pwi@ihtsdo.org>
2015-11-05 13:45:00 +0000

    RestyHelper to do best job it can when failure detected, without causing futher issues

--- src/main/java/org/ihtsdo/otf/rest/client/resty/RestyServiceHelper.java
+++ src/main/java/org/ihtsdo/otf/rest/client/resty/RestyServiceHelper.java
@@ -53,23 +53,34 @@ public class RestyServiceHelper {
 		}
 	}
 
-	private static void throwAppropriateException(JSONResource response) throws JSONException, IOException, BusinessServiceException {
+	private static void throwAppropriateException(JSONResource response) throws BusinessServiceException{
 		// If we can, work out what happened in the response and throw something appropriate
-		JSONObject jsonObj = response.object();
-		String exceptionMessage = jsonObj.toString(2);
+		String exceptionMessage = "Uable to obtain error message from response";
+		JSONObject jsonObj = null;
+		
+		try {
+			jsonObj = response.object();
+			exceptionMessage = jsonObj.toString(2);
+		} catch (IOException|JSONException e) {
+			exceptionMessage += " due to " + e.getMessage();
+		}
 
 		// Can we get the http status directly from the object?
 		Integer httpStatus = response.getHTTPStatus();
 
 		// Othewise try and parse one out of the response body
-		if (httpStatus == null && jsonObj.has(HTTP_STATUS)) {
-			httpStatus = Integer.parseInt(jsonObj.getString(HTTP_STATUS));
+		if (jsonObj != null && httpStatus == null && jsonObj.has(HTTP_STATUS)) {
+			try{
+				httpStatus = Integer.parseInt(jsonObj.getString(HTTP_STATUS));
+			} catch (JSONException e) {}
 		}
 
 		if (httpStatus != null) {
 			// Do we also have an error message ? Use that as the exception message if so, otherwise use body
-			if (jsonObj.has(ERROR_MESSAGE)) {
-				exceptionMessage = jsonObj.getString(ERROR_MESSAGE);
+			if (jsonObj != null && jsonObj.has(ERROR_MESSAGE)) {
+				try{
+					exceptionMessage = jsonObj.getString(ERROR_MESSAGE);
+				} catch (JSONException e) {}
 			}
 			switch (httpStatus.intValue()) {
 			case HttpStatus.SC_NOT_FOUND:
