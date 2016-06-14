088d8d7f25659853e210e1c8539a8987331ff434
Kai Kewley <kaikewley@gmail.com>
2016-06-14 10:40:05 +0100

    Fix: Autoclose S3 object streams.

--- src/main/java/org/ihtsdo/otf/dao/s3/S3ClientImpl.java
+++ src/main/java/org/ihtsdo/otf/dao/s3/S3ClientImpl.java
@@ -1,22 +1,16 @@
 package org.ihtsdo.otf.dao.s3;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-
-import org.apache.commons.io.FileUtils;
-
 import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class S3ClientImpl extends AmazonS3Client implements S3Client {
 
@@ -25,18 +19,20 @@ public class S3ClientImpl extends AmazonS3Client implements S3Client {
 	}
 
 	public PutObjectResult putObject(String bucketName, String key, InputStream input, ObjectMetadata metadata)
-			throws AmazonClientException, AmazonServiceException {
+			throws AmazonClientException {
 		// Memory problems with large files necessitate writing to disk before
 		// uploading to AWS
 		File cachedFile = cacheLocally(input, key);
 		PutObjectRequest putRequest = new PutObjectRequest(bucketName, key, cachedFile);
 		putRequest.setMetadata(metadata);
-		PutObjectResult result = super.putObject(putRequest);
-		cachedFile.delete();
-		return result;
+		try {
+			return super.putObject(putRequest);
+		} finally {
+			cachedFile.delete();
+		}
 	}
 
-	public PutObjectResult putObject(PutObjectRequest putRequest) throws AmazonClientException, AmazonServiceException {
+	public PutObjectResult putObject(PutObjectRequest putRequest) throws AmazonClientException {
 		// If we have an inputstream, modify the putRequest with a file of known size instead
 		if (putRequest.getFile() == null && putRequest.getInputStream() != null) {
 			File cachedFile = cacheLocally(putRequest.getInputStream(), putRequest.getKey());
@@ -61,10 +57,7 @@ public class S3ClientImpl extends AmazonS3Client implements S3Client {
 
 	@Override
 	public String getString(String bucketName, String key) {
-		String result = null;
-
-		try {
-			S3Object obj = getObject(bucketName, key);
+		try (S3Object obj = getObject(bucketName, key)) {
 			InputStream is = obj.getObjectContent();
 			BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
 			StringBuilder responseStrBuilder = new StringBuilder();
@@ -74,13 +67,10 @@ public class S3ClientImpl extends AmazonS3Client implements S3Client {
 				responseStrBuilder.append(inputStr);
 			}
 
-			result = responseStrBuilder.toString();
-
+			return responseStrBuilder.toString();
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new AmazonClientException("Failed to load resource.", e);
 		}
-
-		return result;
 	}
 
 }
