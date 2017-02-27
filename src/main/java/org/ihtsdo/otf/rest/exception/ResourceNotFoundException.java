0ab40726793dac2c2804624e34ef2caaad36b5bc
Kai Kewley <kaikewley@gmail.com>
2017-02-27 16:23:27 +0000

    Add Spring HTTP response code to ResourceNotFoundException.

--- src/main/java/org/ihtsdo/otf/rest/exception/ResourceNotFoundException.java
+++ src/main/java/org/ihtsdo/otf/rest/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package org.ihtsdo.otf.rest.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
 public class ResourceNotFoundException extends BusinessServiceRuntimeException {
 
 	private static final long serialVersionUID = -4281549626769059242L;
