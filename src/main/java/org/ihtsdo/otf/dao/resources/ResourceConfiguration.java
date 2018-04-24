9ebfd638a71ff0a056c99e7e4403346394f0802a
Kai Kewley <kaikewley@gmail.com>
2018-04-24 16:57:14 +0100

    Add javadoc for S3 ResourceConfiguration.

--- src/main/java/org/ihtsdo/otf/dao/resources/ResourceConfiguration.java
+++ src/main/java/org/ihtsdo/otf/dao/resources/ResourceConfiguration.java
@@ -1,6 +1,19 @@
 package org.ihtsdo.otf.dao.resources;
 
-public class ResourceConfiguration {
+/**
+ * Use this configuration class by extending it.
+ * Create a class which extends ResourceConfiguration and add the Spring 'Configuration' and 'ConfigurationProperties' annotations.
+ * The ConfigurationProperties annotation should include a prefix for your resource configuration.
+ * For example a TasksResourceConfiguration with a prefix of 'tasks.storage' would load the following properties from your configuration:
+ * - tasks.storage.readonly
+ * - tasks.storage.useCloud
+ * - tasks.storage.local.path
+ * - tasks.storage.cloud.bucketName
+ * - tasks.storage.cloud.path
+ *
+ * TasksResourceConfiguration would be autowired into your spring configuration and then passed to the constructor of ResourceManager.
+ */
+public abstract class ResourceConfiguration {
 
 	private boolean readonly;
 	private boolean useCloud;
