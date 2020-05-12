85d81aee007580c416ca770b486ab6687e6a4967
Kai Kewley <kaikewley@gmail.com>
2020-05-12 12:15:09 +0100

    MAINT-849 Use just conceptId field in hashcode and equals of ConceptMini.

--- otf-common/src/main/java/org/ihtsdo/otf/rest/client/terminologyserver/pojo/ConceptMiniPojo.java
+++ otf-common/src/main/java/org/ihtsdo/otf/rest/client/terminologyserver/pojo/ConceptMiniPojo.java
@@ -2,6 +2,8 @@ package org.ihtsdo.otf.rest.client.terminologyserver.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Objects;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ConceptMiniPojo {
 	
@@ -76,48 +78,18 @@ public class ConceptMiniPojo {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((conceptId == null) ? 0 : conceptId.hashCode());
-		result = prime * result + ((definitionStatus == null) ? 0 : definitionStatus.hashCode());
-		result = prime * result + ((fsn == null) ? 0 : fsn.hashCode());
-		result = prime * result + ((moduleId == null) ? 0 : moduleId.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ConceptMiniPojo that = (ConceptMiniPojo) o;
+		return Objects.equals(conceptId, that.conceptId);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ConceptMiniPojo other = (ConceptMiniPojo) obj;
-		if (conceptId == null) {
-			if (other.conceptId != null)
-				return false;
-		} else if (!conceptId.equals(other.conceptId))
-			return false;
-		if (definitionStatus == null) {
-			if (other.definitionStatus != null)
-				return false;
-		} else if (!definitionStatus.equals(other.definitionStatus))
-			return false;
-		if (fsn == null) {
-			if (other.fsn != null)
-				return false;
-		} else if (!fsn.equals(other.fsn))
-			return false;
-		if (moduleId == null) {
-			if (other.moduleId != null)
-				return false;
-		} else if (!moduleId.equals(other.moduleId))
-			return false;
-		return true;
+	public int hashCode() {
+		return Objects.hash(conceptId);
 	}
-	
+
 	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static class DescriptionMiniPojo {
 		private String term;
