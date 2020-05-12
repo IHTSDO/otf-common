9e93123fac04b6b837981757ec7911bd4ae3e97b
Kai Kewley <kaikewley@gmail.com>
2020-05-12 12:14:58 +0100

    MAINT-849 Prevent authentication exception exposing stack trace.

--- otf-common/src/main/java/org/ihtsdo/otf/rest/exception/AuthenticationException.java
+++ otf-common/src/main/java/org/ihtsdo/otf/rest/exception/AuthenticationException.java
@@ -1,8 +1,22 @@
 package org.ihtsdo.otf.rest.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class AuthenticationException extends UncheckedBusinessServiceException {
 
 	public AuthenticationException(String message) {
 		super(message);
+	}
+
+	@Override
+	@JsonIgnore
+	public StackTraceElement[] getStackTrace() {
+		return super.getStackTrace();
+	}
+
+	@Override
+	@JsonIgnore
+	public synchronized Throwable getCause() {
+		return super.getCause();
 	}
 }
