2cc3887203403e49970f975e66ec2d646875abfd
CoderMChu <mch@ihtsdo.org>
2024-09-04 09:15:32 +0100

    PIP-540 Address security issues in FileUtils as reported by sonar report

--- common-utils/src/main/java/org/ihtsdo/otf/utils/FileUtils.java
+++ common-utils/src/main/java/org/ihtsdo/otf/utils/FileUtils.java
@@ -23,24 +23,32 @@ public class FileUtils {
 
 	private static final String MD5 = "MD5";
 
+
+	private static final int THRESHOLD_ENTRIES = 10000;
+
+
+	private FileUtils() {
+		// private constructor to prevent instantiation
+	}
+
 	public static Map<String, String> examineZipContents(final String filename, final InputStream is) {
-		//TODO Option to try treating this stream as GZip (GZipInputStream) also.
 		Map<String, String> contents = new HashMap<>();
-		try {
-			ZipInputStream zis = new ZipInputStream(is);
-			ZipEntry entry = zis.getNextEntry();
+		try (ZipInputStream zis = new ZipInputStream(is)) {
+			ZipEntry entry;
 			int idx = 0;
-			while (entry != null) {
+			while ((entry = zis.getNextEntry()) != null) {
 				contents.put("zip_content_" + idx, entry.getName());
-				LOGGER.debug(filename + "[" + idx + "]: " + entry.getName());
-				entry = zis.getNextEntry();
+				LOGGER.debug("{}[{}]: {}", filename, idx, entry.getName());
 				idx++;
+				// Check for zip bombs
+				if (idx > THRESHOLD_ENTRIES) {
+					LOGGER.error("Zip bomb detected in file: {}", filename);
+					break;
+				}
 			}
 		} catch (Exception e) {
-			LOGGER.debug("Failed to enumerate zip file contents", e);
+			LOGGER.error("Failed to enumerate zip file contents", e);
 		}
-
-
 		return contents;
 	}
 
@@ -48,45 +56,37 @@ public class FileUtils {
 	 *@author http://www.mkyong.com/java/how-to-generate-a-file-checksum-value-in-java/
 	 */
 	public static String calculateMD5(final File file) throws NoSuchAlgorithmException, IOException {
-		StringBuilder sb = new StringBuilder("");
+		StringBuilder sb = new StringBuilder();
 		MessageDigest md = MessageDigest.getInstance(MD5);
-		FileInputStream fis = new FileInputStream(file);
-		byte[] dataBytes = new byte[KB];
-
-		int bytesRead;
-		while ((bytesRead = fis.read(dataBytes)) != -1) {
-			md.update(dataBytes, 0, bytesRead);
+		try (FileInputStream fis = new FileInputStream(file)) {
+			byte[] dataBytes = new byte[KB];
+			int bytesRead;
+			while ((bytesRead = fis.read(dataBytes)) != -1) {
+				md.update(dataBytes, 0, bytesRead);
+			}
+			//convert the byte to hex format
+			byte[] md5Bytes = md.digest();
+			for (byte md5Byte : md5Bytes) {
+				sb.append(Integer.toString((md5Byte & 0xff) + 0x100, 16).substring(1));
+			}
 		}
-
-		//convert the byte to hex format
-		byte[] md5Bytes = md.digest();
-		for (byte md5Byte : md5Bytes) {
-			sb.append(Integer.toString((md5Byte & 0xff) + 0x100, 16).substring(1));
-		}
-		fis.close();
 		return sb.toString();
 	}
 
 	/**
 	 * Creates a file in the same directory as hashMe, using the same name with .md5 appended to it.
 	 *
-	 * @param hashMe
-	 * @return
-	 * @throws IOException
-	 * @throws NoSuchAlgorithmException
+	 * @param hashMe   the file to be hashed
+	 * @return the created MD5 file
+	 * @throws IOException if the file cannot be created
 	 */
-	public static File createMD5File(final File hashMe, String md5String) throws NoSuchAlgorithmException, IOException {
+	public static File createMD5File(final File hashMe, String md5String) throws IOException {
 		String resultFilePath = hashMe.getAbsolutePath() + MD5_EXTENSION;
-
 		File resultFile = new File(resultFilePath);
-		FileOutputStream fop = new FileOutputStream(resultFile);
-
-		byte[] contentInBytes = md5String.getBytes();
-
-		fop.write(contentInBytes);
-		fop.flush();
-		fop.close();
-
+		try (FileOutputStream fop = new FileOutputStream(resultFile)) {
+			byte[] contentInBytes = md5String.getBytes();
+			fop.write(contentInBytes);
+		}
 		return resultFile;
 	}
 
