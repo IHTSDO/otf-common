e32cb0ae7438eb2c67b4d84cbe003a501af8397a
pgwilliams <petergwilliams@hotmail.com>
2024-10-04 12:04:36 +0100

    RP-938 Improve detail of debug logging and SonarQube score

--- resource-manager/src/main/java/org/snomed/otf/script/Script.java
+++ resource-manager/src/main/java/org/snomed/otf/script/Script.java
@@ -23,9 +23,6 @@ public abstract class Script implements RF2Constants {
 	protected static final String REPORT_OUTPUT_TYPES = "ReportOutputTypes";
 	protected static final String REPORT_FORMAT_TYPE = "ReportFormatType";
 
-	@Deprecated(forRemoval = true)
-	protected Logger logger = LoggerFactory.getLogger(this.getClass());
-
 	public static List<String> HISTORICAL_REFSETS = new ArrayList<>();
 	static {
 		HISTORICAL_REFSETS.add(SCTID_ASSOC_WAS_A_REFSETID);
