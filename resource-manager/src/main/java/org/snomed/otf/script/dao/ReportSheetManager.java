7a7396720e5d57212dea38a17e141538c5879e35
Peter Groves Williams <petergwilliams@hotmail.com>
2024-10-23 09:25:09 +0900

    RP-961 Remove vulnerability encountered when reporting error in writing to sheet

--- resource-manager/src/main/java/org/snomed/otf/script/dao/ReportSheetManager.java
+++ resource-manager/src/main/java/org/snomed/otf/script/dao/ReportSheetManager.java
@@ -403,7 +403,8 @@ public class ReportSheetManager implements RF2Constants, ReportProcessor {
 				}
 			} catch (IOException e) {
 				//Output some sample of the data so get an idea of where it's coming from.
-				LOGGER.info("Last data attempted: {}", dataToBeWritten.toString().substring(0, 250));
+				int dataCutPoint = Math.min(dataToBeWritten.size(), 250);
+				LOGGER.info("Last data attempted: {}", dataToBeWritten.toString().substring(0, dataCutPoint));
 				throw new TermServerScriptException("Unable to update spreadsheet " + sheet.getSpreadsheetUrl(), e);
 			} finally {
 				lastWriteTime = new Date();
