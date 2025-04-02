673fb52366ae6287573263033e21d709787d61d2
Quyen Ly <quocquyenit@gmail.com>
2025-04-02 16:10:26 +0700

    MAINT-2761 Address Sonar feedback

--- common-domain/src/main/java/org/ihtsdo/otf/rest/client/terminologyserver/pojo/AnnotationPojo.java
+++ common-domain/src/main/java/org/ihtsdo/otf/rest/client/terminologyserver/pojo/AnnotationPojo.java
@@ -5,22 +5,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class AnnotationPojo implements SnomedComponent {
-
+public class AnnotationPojo extends RefsetMemberPojo implements SnomedComponent {
     private String annotationId;
 
-    private boolean active;
-
-    private String effectiveTime;
-
-    private boolean released;
-
-    private String moduleId;
-
     private String languageDialectCode;
-
-    private String referencedComponentId;
-    private String refsetId;
 
     private String typeId;
 
@@ -44,37 +32,6 @@ public class AnnotationPojo implements SnomedComponent {
         this.annotationId = annotationId;
     }
 
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getEffectiveTime() {
-        return effectiveTime;
-    }
-
-    public void setEffectiveTime(String effectiveTime) {
-        this.effectiveTime = effectiveTime;
-    }
-
-    public boolean isReleased() {
-        return released;
-    }
-
-    public void setReleased(boolean released) {
-        this.released = released;
-    }
-
-    public String getModuleId() {
-        return moduleId;
-    }
-
-    public void setModuleId(String moduleId) {
-        this.moduleId = moduleId;
-    }
 
     public String getLanguageDialectCode() {
         return languageDialectCode;
@@ -84,21 +41,6 @@ public class AnnotationPojo implements SnomedComponent {
         this.languageDialectCode = languageDialectCode;
     }
 
-    public String getReferencedComponentId() {
-        return referencedComponentId;
-    }
-
-    public void setReferencedComponentId(String referencedComponentId) {
-        this.referencedComponentId = referencedComponentId;
-    }
-
-    public String getRefsetId() {
-        return refsetId;
-    }
-
-    public void setRefsetId(String refsetId) {
-        this.refsetId = refsetId;
-    }
 
     public String getTypeId() {
         return typeId;
@@ -120,25 +62,25 @@ public class AnnotationPojo implements SnomedComponent {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof AnnotationPojo that)) return false;
-        return isActive() == that.isActive() && isReleased() == that.isReleased() && Objects.equals(getAnnotationId(), that.getAnnotationId()) && Objects.equals(getEffectiveTime(), that.getEffectiveTime()) && Objects.equals(getModuleId(), that.getModuleId()) && Objects.equals(getLanguageDialectCode(), that.getLanguageDialectCode()) && Objects.equals(getReferencedComponentId(), that.getReferencedComponentId()) && Objects.equals(getRefsetId(), that.getRefsetId()) && Objects.equals(getTypeId(), that.getTypeId()) && Objects.equals(getValue(), that.getValue());
+        return isActive() == that.isActive() && getReleased() == that.getReleased() && Objects.equals(getAnnotationId(), that.getAnnotationId()) && Objects.equals(getEffectiveTime(), that.getEffectiveTime()) && Objects.equals(getModuleId(), that.getModuleId()) && Objects.equals(getLanguageDialectCode(), that.getLanguageDialectCode()) && Objects.equals(getReferencedComponentId(), that.getReferencedComponentId()) && Objects.equals(getRefsetId(), that.getRefsetId()) && Objects.equals(getTypeId(), that.getTypeId()) && Objects.equals(getValue(), that.getValue());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getAnnotationId(), isActive(), getEffectiveTime(), isReleased(), getModuleId(), getLanguageDialectCode(), getReferencedComponentId(), getRefsetId(), getTypeId(), getValue());
+        return Objects.hash(getAnnotationId(), isActive(), getEffectiveTime(), getReleased(), getModuleId(), getLanguageDialectCode(), getReferencedComponentId(), getRefsetId(), getTypeId(), getValue());
     }
 
     @Override
     public String toString() {
         return "AnnotationPojo{" +
                 "annotationId='" + annotationId + '\'' +
-                ", active=" + active +
-                ", effectiveTime='" + effectiveTime + '\'' +
-                ", released=" + released +
-                ", moduleId='" + moduleId + '\'' +
+                ", active=" + isActive() +
+                ", effectiveTime='" + getEffectiveTime() + '\'' +
+                ", released=" + getReleased() +
+                ", moduleId='" + getModuleId() + '\'' +
                 ", languageDialectCode='" + languageDialectCode + '\'' +
-                ", referencedComponentId='" + referencedComponentId + '\'' +
-                ", refsetId='" + refsetId + '\'' +
+                ", referencedComponentId='" + getReferencedComponentId() + '\'' +
+                ", refsetId='" + getRefsetId() + '\'' +
                 ", typeId='" + typeId + '\'' +
                 ", value='" + value + '\'' +
                 '}';
