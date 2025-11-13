98fe15aa46e070dc6fcc2e35c13bcba27afa3823
Quyen Ly <quocquyenit@gmail.com>
2025-11-13 14:00:22 +0700

    PIP-908 Address Sonar feedback after switching to new spring-boot version 3.5.7

--- otf-common/src/main/java/org/ihtsdo/otf/rest/client/ExpressiveErrorHandler.java
+++ otf-common/src/main/java/org/ihtsdo/otf/rest/client/ExpressiveErrorHandler.java
@@ -3,6 +3,7 @@ package org.ihtsdo.otf.rest.client;
 import org.ihtsdo.otf.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestClientResponseException;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestClientResponseException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.stream.Collectors;
 
 public class ExpressiveErrorHandler extends DefaultResponseErrorHandler {
@@ -17,7 +19,7 @@ public class ExpressiveErrorHandler extends DefaultResponseErrorHandler {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
-	public void handleError(ClientHttpResponse response) {
+	public void handleError(URI url, HttpMethod method, ClientHttpResponse response) {
 		// Recover error code and message from response
 		int statusCode = 0;
 		String statusText = "";
